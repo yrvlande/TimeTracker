d6d8633fb2839fd5be59dbd17fd94b6f9f2f81fb
Yashwant Lande <yashwant.lande@fisglobal.com>
2022-02-19 11:11:38 +0530

    TimeTracker: Adhoc changes

--- timeTrackerAgent/dataPublisherScheduler.py
+++ timeTrackerAgent/dataPublisherScheduler.py
@@ -2,10 +2,14 @@
 import schedule
 import time
 import requests
+from datetime import datetime
+import calendar;
+from datetime import timezone
 
 
 def publishData():
-    requestJson = {"userId": "e1078064", "mouseClickTime": "2022-02-17T13:10:17", "xCoordinate": 10, "yCoordinate": 11}
+    requestJson = {"id": 4, "userId": "e1078064", "mouseClickTime": "2022-02-17T13:10:17", "xCoordinate": 10,
+                   "yCoordinate": 11, "insertedOn": round(time.time() * 1000)}
     response = requests.post("http://localhost:8087/event/data", json=requestJson)
 
 
