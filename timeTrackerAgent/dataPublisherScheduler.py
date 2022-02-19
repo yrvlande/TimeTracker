b63a39def012e7b29b15eb1e2232274f15ee643d
Yashwant Lande <yashwant.lande@fisglobal.com>
2022-02-19 11:52:31 +0530

    Time Tracker: Handled server connection exception

--- timeTrackerAgent/dataPublisherScheduler.py
+++ timeTrackerAgent/dataPublisherScheduler.py
@@ -5,9 +5,14 @@ import requests
 
 
 def publishData():
-    requestJson = {"id": 5, "userId": "e1078064", "mouseClickTime": "2022-02-17T13:10:17", "xCoordinate": 10,
+    requestJson = {"id": 6, "userId": "e1078064", "mouseClickTime": "2022-02-17T13:10:17", "xCoordinate": 10,
                    "yCoordinate": 11}
-    response = requests.post("http://localhost:8087/event/data", json=requestJson)
+    try:
+        response = requests.post("http://localhost:8087/event/data", json=requestJson)
+        print(response.status_code)
+        print(response.json())
+    except:
+        print("Connection Refused")
 
 
 schedule.every(10).seconds.do(publishData)
