439c30bb942fe621891adb0543fe2c2e8af1eb43
Nishant Verma <nvermavermanishant@gmail.com>
2022-02-21 11:24:25 +0530

    TimeTracker: Added logic to read keyboard also and repeated data will be sent only once.

--- timeTrackerAgent/dataPublisherScheduler.py
+++ timeTrackerAgent/dataPublisherScheduler.py
@@ -5,25 +5,18 @@ import requests
 import os
 
 lastSentData = ''
-isDataPublishOFF = False
 def publishData():
     with open("mouse_log.txt", "r") as file:
         for last_line in file:
             pass
     trimmedLastLine = last_line.strip()
-    global lastSentData, isDataPublishOFF
-    print('Pre--' + lastSentData)
-    print('isDataPublishOFF=', isDataPublishOFF)
-    if not trimmedLastLine.__eq__(lastSentData) or not isDataPublishOFF:
-        print(isDataPublishOFF, " Working")
-        print('lastSentData=' + lastSentData + ' and ' + 'trimmedLastLine=' + trimmedLastLine)
+    global lastSentData
+    print('lastSentData=' + lastSentData + ' and ' + 'trimmedLastLine=' + trimmedLastLine)
+    if not trimmedLastLine.__eq__(lastSentData):
+        print("Sending data ON")
         spiltedstampandcoord = trimmedLastLine.split("&")
         coordinates = spiltedstampandcoord[1].strip().split(":")
         requestJson = {"userId": os.getlogin(), "mouseClickTime": spiltedstampandcoord[0], "xCoordinate": coordinates[0], "yCoordinate": coordinates[1]}
-        if trimmedLastLine.__eq__(lastSentData):
-            isDataPublishOFF = True
-        else:
-            isDataPublishOFF = False
         lastSentData = trimmedLastLine
         try:
             response = requests.post("http://localhost:8087/event/data", json=requestJson)
@@ -33,7 +26,13 @@ def publishData():
             print("Connection Refused")
 
 
+def clean_file():
+    print("A Day Only")
+    open("mouse_log.txt", "r+").truncate(0)
+
+
 schedule.every(3).seconds.do(publishData)
+
 
 while True:
     schedule.run_pending()
